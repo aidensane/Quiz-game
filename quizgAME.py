1d5ede185fa4ea9c32b76ea9a0d91c36be3153a5
aidensane <98586059+aidensane@users.noreply.github.com>
2022-12-01 10:12:26 +0100

    added a score counter

--- quizgAME.py
+++ quizgAME.py
@@ -1,11 +1,14 @@
 print("Welcome to my computer quiz!")
 
-play = input("Do you wanna play my game? ")
+play = input("Do you wanna play my game? ").lower()
 
 if play != "yes":
     quit()
-else:
-    print("Okay, lets play :)")
+
+print("Okay, lets play :)")
+
+score = 0
+
 
 #first question
 
@@ -14,3 +17,22 @@ if ans != "central processing unit":
     print("Incorrect :(")
 else:
     print("Correct!")
+    score += 1
+
+#second question
+ans = input("What does GPU mean? ").lower()
+if ans != "graphics processing unit":
+    print("Incorrect :(")
+else:
+    print("Correct!")
+    score += 1
+
+#third question
+ans = input("What does LCD mean? ").lower()
+if ans != "liquid crystal display":
+    print("Incorrect :(")
+else:
+    print("Correct!")
+    score += 1
+
+print("you got: " + str(score) + " questions correct! :)")
